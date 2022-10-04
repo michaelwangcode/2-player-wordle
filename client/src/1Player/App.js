22783b40809769befb697fb05a984d5aaf62abcd
michaelwangcode <michaelwangxv@gmail.com>
2022-10-04 04:59:39 -0400

    Add Home Page and Single Player Game

---
+++ client/src/1Player/App.js
@@ -0,0 +1,95 @@
+import './1PlayerApp.css';
+import Board from './components/Board';
+import Keyboard from './components/Keyboard';
+import GameOver from './components/GameOver';
+import { createContext, useEffect, useState } from "react";
+import { boardDefault, generateWordSet } from './components/Words';
+
+export const AppContext = createContext();
+
+function App() {
+  const [board, setBoard] = useState(boardDefault);
+  const [currAttempt, setCurrAttempt] = useState({ attempt: 0, letterPos: 0 });
+  const [wordSet, setWordSet] = useState(new Set());
+  const [disabledLetters, setDisabledLetters] = useState([]);
+  const [gameOver, setGameOver] = useState({
+    gameOver: false,
+    guessedWord: false
+  })
+  const [correctWord, setCorrectWord] = useState("")
+
+  useEffect(() => {
+    generateWordSet().then((words) => {
+      setWordSet(words.wordSet);
+      setCorrectWord(words.todaysWord)
+    })
+  }, [])
+
+  const onSelectLetter = (keyVal) => {
+    if (currAttempt.letterPos > 4) return;
+    const newBoard = [...board];
+    newBoard[currAttempt.attempt][currAttempt.letterPos] = keyVal;
+    setBoard(newBoard);
+    setCurrAttempt({ ...currAttempt, letterPos: currAttempt.letterPos += 1 })
+  }
+
+  const onDelete = () => {
+    if (currAttempt.letterPos === 0) return;
+    const newBoard = [...board];
+    newBoard[currAttempt.attempt][currAttempt.letterPos - 1] = "";
+    setBoard(newBoard)
+    setCurrAttempt({ ...currAttempt, letterPos: currAttempt.letterPos -= 1 })
+  }
+
+  const onEnter = () => {
+    if (currAttempt.letterPos !== 5) return;
+
+    let currentWord = "";
+    for (let i = 0; i < 5; i++) {
+      currentWord += board[currAttempt.attempt][i];
+    }
+    if (wordSet.has(currentWord.toLowerCase())) {
+      setCurrAttempt({ attempt: currAttempt.attempt += 1, letterPos: currAttempt.letterPos = 0 })
+    } else {
+      alert("Word not found!")
+    }
+
+    if (currentWord === correctWord) {
+      setGameOver({ gameOver: true, guessedWord: true })
+      return;
+    }
+
+    if (currAttempt.attempt === 6) {
+      setGameOver({ gameOver: true, guessedWord: false })
+    }
+
+  }
+  return (
+    <div className="App">
+      <nav>
+        <h1>Wordle</h1>
+      </nav>
+      <AppContext.Provider value={{
+        board,
+        setBoard,
+        currAttempt,
+        setCurrAttempt,
+        onSelectLetter,
+        onDelete,
+        onEnter,
+        correctWord,
+        setDisabledLetters,
+        disabledLetters,
+        setGameOver,
+        gameOver
+      }}>
+        <div className='game'>
+          <Board />
+          {gameOver.gameOver ? <GameOver /> : <Keyboard />}
+        </div>
+      </AppContext.Provider>
+    </div>
+  );
+}
+
+export default App;
--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,74 @@
 import './App.css';
-import Join from "./Join";
 import { useState } from "react";
 
+import Join from "./Join";
+import OnePlayerApp from "./1Player/App";
 
 
 // The main application component
 function App() {
 
+  // List of different hooks:
+  // 1. Home
+  // 2. Single Player
+  // 3. Two Player
+  // 4. 4 Person Tournament
+  // 5. 8 Person Tournament
+  // 6. Stats
 
-  // Hook that determines whether or not to show the home buttons
-  const [showJoin, setShowJoin] = useState(false);
+  // These hooks are used to hide or display a component
+  const [showHome, setShowHome] = useState(true);
+  const [show1PlayerGame, setShow1PlayerGame] = useState(false);
+  const [show2PlayerGame, setShow2PlayerGame] = useState(false);
+  const [show4PlayerTournament, setShow4PlayerTournament] = useState(false);
+  const [show8PlayerTournament, setShow8PlayerTournament] = useState(false);
+  const [showStats, setShowStats] = useState(false);
 
 
-  const select2PlayerGame = () => {
-
-    console.log("2 Player game selected");
-
-    setShowJoin(true);
+  // This function displays a 1 Player Game
+  const select1PlayerGame = () => {
+    console.log("1 Player game selected");
+    setShowHome(false);
+    setShow1PlayerGame(true);
   };
 
+  // This function displays a 2 Player Game
+  const select2PlayerGame = () => {
+    console.log("2 Player game selected");
+    setShowHome(false);
+    setShow2PlayerGame(true);
+  };
+
+  // This function displays a 4 Player Tournament
+  const select4PlayerTournament = () => {
+    console.log("4 Player tournament selected");
+    setShowHome(false);
+    setShow4PlayerTournament(true);
+  };
+
+  // This function displays an 8 Player Tournament
+  const select8PlayerTournament = () => {
+    console.log("8 Player tournament selected");
+    setShowHome(false);
+    setShow8PlayerTournament(true);
+  };
+
+  // This function displays the user's stats
+  const selectStats = () => {
+    console.log("My Stats");
+    setShowHome(false);
+    setShowStats(true);
+  };
 
   const backToHome = () => {
-
     console.log("Back to home");
-
-    setShowJoin(false);
+    setShow1PlayerGame(false);
+    setShow2PlayerGame(false);
+    setShow4PlayerTournament(false);
+    setShow8PlayerTournament(false);
+    setShowStats(false);
+    setShowHome(true);
   };
-
 
 
 
@@ -35,32 +77,80 @@ function App() {
 
     <div className="App">
       
-      { !showJoin ? (
-
-      <div>
-        <br/><br/>
-        <button><h1>1 player game</h1></button>
-        <br/><br/>
-        <button onClick={select2PlayerGame}><h1>2 player game</h1></button>
-        <br/><br/>
-        <button><h1>4 player tournament</h1></button>
-        <br/><br/>
-        <button><h1>8 player tournament</h1></button>
-      </div>
-      ) 
-      : (
+      {/* If showHome is true, display the Home Page with Buttons */}
+      {showHome && (
         <div>
-          {/* Back to Home Button */}
-          <button onClick={backToHome}><h1>Back</h1></button>
-
-          {/* Display the Join window component */}
-          <Join />
+          <h1>Wordle</h1>
+          <br/><br/>
+          <button onClick={select1PlayerGame}><h1>1 Player Game</h1></button>
+          <br/><br/>
+          <button onClick={select2PlayerGame}><h1>2 Player Game</h1></button>
+          <br/><br/>
+          <button onClick={select4PlayerTournament}><h1>4 Player Tournament</h1></button>
+          <br/><br/>
+          <button onClick={select8PlayerTournament}><h1>8 Player Tournament</h1></button>
+          <br/><br/>
+          <button onClick={selectStats}><h1>My Stats</h1></button>
+          <br/><br/>
         </div>
       )}
 
+      {/* If show1PlayerGame is true, display a 1 Player Game */}
+      {show1PlayerGame && (
+        <div>
+          {/* Back to Home Button */}
+          <button onClick={backToHome}>Back</button>
+
+          {/* Display a 1 Player Game */}
+          <OnePlayerApp />
+        </div>
+      )}
+
+      {/* If show2PlayerGame is true, display a 2 Player Game */}
+      {show2PlayerGame && (
+        <div>
+          {/* Back to Home Button */}
+          <button onClick={backToHome}>Back</button>
+
+          {/* Display the Join window component */}
+          <Join/>
+        </div>
+      )}
+
+      {/* If show4PersonTournament is true, display a 4 Person Tournament */}
+      {show4PlayerTournament && (
+        <div>
+          {/* Back to Home Button */}
+          <button onClick={backToHome}>Back</button>
+
+          <h1>Create a 4 Person Tournament</h1>
+        </div>
+      )}
+
+      {/* If show8PersonTournament is true, display an 8 Person Tournament */}
+      {show8PlayerTournament && (
+        <div>
+          {/* Back to Home Button */}
+          <button onClick={backToHome}>Back</button>
+
+          <h1>Create an 8 Person Tournament</h1>
+        </div>
+      )}
+
+      {/* If showStats is true, display an 8 Person Tournament */}
+      {showStats && (
+        <div>
+          {/* Back to Home Button */}
+          <button onClick={backToHome}>Back</button>
+
+          <h1>My Stats</h1>
+        </div>
+      )}
 
     </div>
   );
 }
+
+
 
 export default App;
